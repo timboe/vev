203de613ed4b43856d362cb29a82f25f5398c6d3
Tim Martin <timboe@gmail.com>
2017-08-20 15:24:17 +0200

    Initial libgdx

---
+++ desktop/src/timboe/destructor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package timboe.destructor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import timboe.destructor.destructor;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new destructor(), config);
+	}
+}
